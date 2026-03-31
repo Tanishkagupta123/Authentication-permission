27cffe6d7c01317755d51a384f170be32ef55034
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-03-31 23:33:38 +0530

    first commit

---
+++ app/routers.py
@@ -0,0 +1,8 @@
+from .views import StudentViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'student', StudentViewSet, basename='students'),
+# router.register(r'employee', StudentViewSet, basename='employee'),
+
+urlpatterns = router.urls
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Student
+from .serializers import StuSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
+from rest_framework import viewsets
+
+
+# class all_data(generics.ListCreateAPIView):
+#     permission_classes = [IsAuthenticated]
+#     queryset = Student.objects.all()
+#     serializer_class = StuSerializer
+
+
+# class data(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StuSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAdminUser]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Student.objects.all()
+    serializer_class = StuSerializer
