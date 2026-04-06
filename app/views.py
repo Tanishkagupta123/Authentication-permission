b101093f6d1c4bd2e699a3bfe4f2624873457652
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-04-06 22:02:11 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -7,12 +7,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 from rest_framework import viewsets
 
-
 # class all_data(generics.ListCreateAPIView):
 #     permission_classes = [IsAuthenticated]
 #     queryset = Student.objects.all()
 #     serializer_class = StuSerializer
-
 
 # class data(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Student.objects.all()
